bf8f46ace5d89baa1987bc120d345a66e2e9a5e4
Anastasia Shuba <ashuba22@gmail.com>
2025-02-13 22:04:03 -0500

    WIP - tokenization done

--- bin/validate_live_pixel.mjs
+++ bin/validate_live_pixel.mjs
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 
+import csv from 'csv-parser';
 import fs from 'fs';
 import JSON5 from 'json5';
+import path from 'path';
 
 import { ParamsValidator } from '../src/params_validator.mjs';
 import { logErrors } from '../src/error_utils.mjs';
@@ -11,38 +13,107 @@ const clickhouseHost = process.env.CLICKHOUSE_HOST;
 
 const args = process.argv.slice(2);
 const mainDir = args[0];
-const pixelDefPath = args[1];
+// const pixelDefPath = args[1];
 const prefix = args[2];
 
 const productDef = JSON5.parse(fs.readFileSync(`${mainDir}/product.json`).toString());
 // Whether to force all schemas and pixels to lowercase
 const forceLowerCase = productDef.forceLowerCase;
 
-const pixelDefs = JSON5.parse(getNormalizedCase(fs.readFileSync(`${mainDir}/pixels/${pixelDefPath}`).toString()));
 const commonParams = JSON5.parse(getNormalizedCase(fs.readFileSync(`${mainDir}/common_params.json`).toString()));
 const ignoreParams = JSON5.parse(getNormalizedCase(fs.readFileSync(`${mainDir}/ignore_params.json`).toString()));
 const commonSuffixes = JSON5.parse(getNormalizedCase(fs.readFileSync(`${mainDir}/common_suffixes.json`).toString()));
 const paramsValidator = new ParamsValidator(commonParams, commonSuffixes);
 
 function main() {
-    console.log(`Processing pixels defined in ${pixelDefPath}`);
+    console.log(`Processing pixels defined in ${mainDir}`);
+    const pixelDefs = readPixelDefs(`${mainDir}/pixels`);
+    // console.log(pixelDefs);
+    // console.log(JSON.stringify(pixelDefs, null, 4));
 
-    if (prefix) {
-        const url = args[3];
+    // TODO: might be bugs in new tab page pixels. Restore deleted pixels
 
-        console.log('Validating', prefix);
-        validateSinglePixel(pixelDefs, prefix, url);
-    } else {
-        if (!clickhouseHost) {
-            console.error('Please set CLICKHOUSE_HOST in your ENV');
-            process.exit(1);
+    readLivePixels(pixelDefs);
+
+    // if (prefix) {
+    //     const url = args[3];
+
+    //     console.log('Validating', prefix);
+    //     validateSinglePixel(pixelDefs, prefix, url);
+    // } else {
+    //     if (!clickhouseHost) {
+    //         console.error('Please set CLICKHOUSE_HOST in your ENV');
+    //         process.exit(1);
+    //     }
+    //     const pixelQueryResults = queryClickhouse(pixelDefs);
+    //     for (const prefix of Object.keys(pixelQueryResults)) {
+    //         console.log('Validating', prefix);
+    //         validateQueryForPixels(prefix, pixelQueryResults[prefix], paramsValidator);
+    //     }
+    // }
+}
+
+function readPixelDefs(folder) {
+    const pixelDefs = {};
+    fs.readdirSync(folder, { recursive: true }).forEach((file) => {
+        const fullPath = path.join(folder, file);
+        if (fs.statSync(fullPath).isDirectory()) {
+            return;
         }
-        const pixelQueryResults = queryClickhouse(pixelDefs);
-        for (const prefix of Object.keys(pixelQueryResults)) {
-            console.log('Validating', prefix);
-            validateQueryForPixels(prefix, pixelQueryResults[prefix], paramsValidator);
+
+        console.log(`Reading pixel def file: ${fullPath}`);
+        const filePixes = JSON5.parse(getNormalizedCase(fs.readFileSync(fullPath).toString()));
+        for (const prefix of Object.keys(filePixes)) {
+            const prefixParts = prefix.split('.');
+            console.log(prefixParts);
+
+            var pixelParent = pixelDefs;
+            for (var i = 0; i < prefixParts.length-1; i++) {
+                const part = prefixParts[i];
+                if (!pixelParent[part]) {
+                    pixelParent[part] = {};
+                }
+                pixelParent = pixelParent[part];
+            }
+            
+            const lastPart = prefixParts[prefixParts.length-1];
+            if (pixelParent[lastPart]) {
+                pixelParent[lastPart][''] = filePixes[prefix];
+            } else {
+                pixelParent[lastPart] = {'': filePixes[prefix]};
+            }
         }
-    }
+        //process.exit(1);
+    });
+
+    return pixelDefs;
+}
+
+function readLivePixels(pixelDefs) {
+    fs.createReadStream('test.csv')
+        .pipe(csv())
+        .on('data', (row) => {
+            const pixelParts = row.pixel.split('.');
+            var pixelMatch = pixelDefs;
+            var matchedParts = "";
+            for (var i = 0; i < pixelParts.length; i++) {
+                const part = pixelParts[i];
+                if (pixelMatch[part]) {
+                    pixelMatch = pixelMatch[part];
+                    matchedParts += part + ".";
+                } else {
+                    break;
+                }
+            }
+
+            // TODO: at this point match pixel against '' value
+            console.log(row.pixel);
+            console.log(matchedParts);
+            console.log(pixelMatch);
+        })
+        .on('end', async () => {
+            console.log('CSV file successfully processed');
+        });
 }
 
 function queryClickhouse(pixelDefs) {
