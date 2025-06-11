fd0b6f036781ea92cfa652525b3e6f40633dad39
Jeanna Matthews <jmatthews@duckduckgo.com>
2025-06-11 05:47:56 -0400

    Change limit from 28 to 7 days

--- live_validation_scripts/validate_live_pixel.mjs
+++ live_validation_scripts/validate_live_pixel.mjs
@@ -48,6 +48,14 @@ function main(mainDir, csvFile) {
                 fileUtils.getUndocumentedPixelsPath(mainDir),
                 JSON.stringify(Array.from(liveValidator.undocumentedPixels), null, 4),
             );
+
+            /*
+                This script will fail if there are too many errors to write out the JSON.
+                For now we are limiting the validation to the last 7 days in 
+                clickhouse_fetcher.mjs and that keeps the JSON at an acceptable size. 
+                Long term we can revisit this for a more robust solution.
+
+            */
             fs.writeFileSync(fileUtils.getPixelErrorsPath(mainDir), JSON.stringify(liveValidator.pixelErrors, setReplacer, 4));
             console.log(`Validation results saved to ${fileUtils.getResultsDir(mainDir)}`);
         });
--- src/clickhouse_fetcher.mjs
+++ src/clickhouse_fetcher.mjs
@@ -7,6 +7,7 @@ import { readTokenizedPixels, readProductDef } from './file_utils.mjs';
 const MAX_MEMORY = 2 * 1024 * 1024 * 1024; // 2GB
 const TMP_TABLE_NAME = 'temp.pixel_validation';
 const CH_ARGS = [`--max_memory_usage=${MAX_MEMORY}`, '-h', 'clickhouse', '--query'];
+const DAYS_TO_FETCH = 7; // Number of days to fetch pixels for
 
 function createTempTable() {
     const queryString = `CREATE TABLE ${TMP_TABLE_NAME}
@@ -41,7 +42,7 @@ function populateTempTable(tokenizedPixels, productDef) {
     const agentWhereClause = productDef.agents.map((agent) => `agent = '${agent}'`).join(' OR ');
 
     const currentDate = new Date();
-    const pastDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() - 28);
+    const pastDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() - DAYS_TO_FETCH);
     /* eslint-disable no-unmodified-loop-condition */
     while (pastDate <= currentDate) {
         const queryString = `INSERT INTO ${TMP_TABLE_NAME} (pixel, params)
