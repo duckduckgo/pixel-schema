b4a570d4e9ce1c405aa90011a33bf456fae5a95b
Lucas Adamski <ladamski@duckduckgo.com>
2025-10-29 06:45:24 -0400

    turns out search_experiments don't prepend exp_ to pixel IDs

--- src/pixel_utils.mjs
+++ src/pixel_utils.mjs
@@ -2,20 +2,19 @@ export function parseSearchExperiments(searchExperiments = {}) {
     const out = {};
 
     for (const [name, def] of Object.entries(searchExperiments)) {
-        const pixelName = `exp_${name}`;
-        out[pixelName] = {
-            key: pixelName,
+        out[name] = {
+            key: name,
             description: def.description,
         };
 
         let type = 'string';
         if (Array.isArray(def?.variants)) {
-            out[pixelName].enum = def.variants;
+            out[name].enum = def.variants;
             if (def.variants.length > 0) {
                 type = typeof def.variants[0];
             }
         }
-        out[pixelName].type = type;
+        out[name].type = type;
     }
 
     return out;
